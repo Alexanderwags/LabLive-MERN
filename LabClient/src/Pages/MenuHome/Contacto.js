32b24f5179fabed2af91219152a57e7ac4061bcc
william_ws7 <alexander171296@gmail.com>
2020-04-26 21:44:40 -0400

    total

--- LabClient/src/Pages/MenuHome/Contacto.js
+++ LabClient/src/Pages/MenuHome/Contacto.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Contacto() {
     return (
-        <div>
+        <>
             
-        </div>
+        </>
     )
 }
 
--- LabClient/src/Pages/MenuHome/QuienesSomos.js
+++ LabClient/src/Pages/MenuHome/QuienesSomos.js
@@ -3,14 +3,22 @@ import Box from '../../BoxTextImg'
 import info from '../InfoPages/QuienesSomos.json'
 function QuienesSomos() {
     return (
-        <>
-  
-          <Box >
-           <img src={require('../../assets/img/calidad.jpg')} alt="jsfkh"/> 
-          </Box>
-
-        </>
-    )
+      <>
+         <h1>Quienes Somos</h1>
+        {
+          info.map((inf) =>{
+           return (
+             <Box text={inf.text} key={inf.id}>
+               <img src={require("../../assets/img/calidad.jpg")} alt="hg" />
+             </Box>
+           );
+          })
+        
+        }
+        
+      </>
+    );
 }
 
 export default QuienesSomos
+
